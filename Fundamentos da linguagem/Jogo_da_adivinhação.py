76eda9447c471e2742d66634ddefb5923ac86dc8
Enzo <enzogabriel8515@gmail.com>
2025-11-21 11:44:20 -0300

    Desafio do jogo da adivinhação
    Aprendi a usar a biblioteca random, para gerar números aleatórios e além disso descobri a importância de usar variáveis booleanas para manter o código limpo.

--- Fundamentos da linguagem/Jogo_da_adivinhação.py
+++ Fundamentos da linguagem/Jogo_da_adivinhação.py
@@ -1,8 +1,8 @@
 import random 
 segredo = random.randint(1,50)
 
-print("\nJogo da adivinhação, teste sua intuição!\n")
-print(" Você têm 5 tentativas! ( ͡° ͜ʖ ͡°)\n ")
+print(" Jogo da adivinhação, teste sua intuição! ")
+print(" Você têm 5 tentativas! ( ͡° ͜ʖ ͡°) ")
 
 chances = 5 
 tent = 1 # Nº de tentativas
@@ -13,18 +13,18 @@ while(not acertou and tent<=chances):
   chute = int(input(f"{tent}º tentativa: "))
 
   if (chute>segredo):
-    print(" Errou! O chute foi maior que o segredo!\n")
+    print(" Errou! O chute foi maior que o segredo!")
     tent+=1
   elif (chute<segredo): 
-    print(" Errou! O chute foi menor que o segredo!\n")
+    print(" Errou! O chute foi menor que o segredo!")
     tent+=1
   else:
-    print(f"Parabéns, você acertou depois de {tent} tentativas (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ \n")
+    print(f"Parabéns, você acertou depois de {tent} tentativas (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ")
     acertou = True #Interrompe o laço quando o usuário acerta
 
 if(not acertou):
-  print(" Você não descobriu o segredo (ಠ_ಠ) \n")
-  print(f" O segredo era: {segredo} (ʘ ͜ʖ ʘ) \n")
+  print(" Você não descobriu o segredo (ಠ_ಠ) ")
+  print(f" O segredo era: {segredo} (ʘ ͜ʖ ʘ) ")
 
-print(" Fim do programa! ( ͡~ ͜ʖ ͡°)\n")
+print(" Fim do programa! ( ͡~ ͜ʖ ͡°)")
 
