d4a9f874f333af5d5a7e773d6f1c39d8ea5c8290
Enzo <enzogabriel8515@gmail.com>
2025-11-25 23:16:50 -0300

    Pecorre as chaves e os valores do dicionário

---
+++ Fundamentos da linguagem/Dicionários/Pecorrendo_dicionario.py
@@ -0,0 +1,11 @@
+alunos = {
+    "Ana Júlia": 12,
+    "Estevão": 13,
+    "Gabriela":14,
+    "Ulisses": 12
+}
+
+print("\n")
+
+for nome,idade in alunos.items():
+    print(f" {nome} têm {idade} anos")
