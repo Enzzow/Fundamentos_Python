139de309a79bdffa1dc614eed35f9b2e837be544
Enzo <enzogabriel8515@gmail.com>
2025-11-28 17:15:40 -0300

    Atualiza o valor da chave

--- Fundamentos da linguagem/Dicionários/Atualiza_dicionario.py
+++ Fundamentos da linguagem/Dicionários/Atualiza_dicionario.py
@@ -3,6 +3,6 @@ pessoa = {
     "idade": 45,
 }
 
-pessoa["idade"] = 50 #Atualiza a idade do Van Sulivan
+pessoa["idade"] = 50 # Atualiza a idade do Van Sulivan
 pessoa["profissão"] = "Escritor" #Adiciona a chave e o valor do dicionário
 
